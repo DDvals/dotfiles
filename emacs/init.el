ddbb2f256057f1bdc927fd09e082588e105b1d3f
Daniele Valsecchi <dvalsecchi@softeam.local>
2026-02-26 16:35:26 +0100

    removed useless init packages

--- emacs/init.el
+++ emacs/init.el
@@ -16,6 +16,49 @@
 
 ;; Set up the visible bell
 (setq visible-bell t)
+
+;; Switch off sounds
+(setq ring-bell-function 'ignore)
+
+(set-face-attribute 'default nil :font "Hack Nerd Font Mono" :height 110)
+
+(load-theme 'wombat)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Move customization variables to a separate file and load it
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+ 
+;; Revert Dired and other buffers
+(setq global-auto-revert-non-file-buffers t)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1 )
+  :custom ((doom-modeline-height 5)))
+
 ;; Switch off sounds
 (setq ring-bell-function 'ignore)
 
